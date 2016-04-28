213c15a55a3ed8d032a9a6b53936e10af2dbdc26
nvanheertum <n.vanheertum@gmail.com>
2016-04-28 12:31:18 +0200

    updating forms.py register
    Checking identation

--- WebAppSofie/viaSofie/forms.py
+++ WebAppSofie/viaSofie/forms.py
@@ -16,12 +16,12 @@ class MyRegistrationForm(UserCreationForm):
 		fields = ('email', 'username', 'password1', 'password2', 'phonenumber', 'firstname', 'lastname')
 
 	def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2:
-            if password1 != password2:
-                raise forms.ValidationError(_("The two password fields didn't match."))
-        return password2
+		password1 = self.cleaned_data.get('password1')
+		password2 = self.cleaned_data.get('password2')
+		if password1 and password2:
+			if password1 != password2:
+				raise forms.ValidationError(_("The two password fields didn't match."))
+		return password2
 
 	#overwriting the save method for custom fields
 	def save(self, commit=True):
