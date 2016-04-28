8c6001f2e5b40f6f6c5c9491f3e949cfff58c5f1
nvanheertum <n.vanheertum@gmail.com>
2016-04-28 12:53:31 +0200

    Updating forms.py regsiter
    Changing import for translation to correct one

--- WebAppSofie/viaSofie/forms.py
+++ WebAppSofie/viaSofie/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from tools.translate import _
+from django.utils.translation import gettext as _
 
 class MyRegistrationForm(UserCreationForm):
 	email = forms.EmailField(required=True)
