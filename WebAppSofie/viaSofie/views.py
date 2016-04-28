d1473535e75190c2a601bd5f75c287461e357422
sulidude <sulidude@hotmail.com>
2016-04-28 13:36:03 +0200

    updated
    added index controller

--- WebAppSofie/viaSofie/views.py
+++ WebAppSofie/viaSofie/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect #handles redirects
 from django.contrib import auth #handles the authantication
 from django.contrib.auth.forms import UserCreationForm
 from django.core.context_processors import csrf #anti crosssite scripting
-from forms import MyRegistrationForm
+
+def index(request):
+	return render_to_response('index.html')
 
 def login(request):
 	c = {}
@@ -37,15 +39,15 @@ def logout(request):
 
 def register_user(request):
 	if request.method == 'POST':
-		form = MyRegistrationForm(request.POST)
+		form = UserCreationForm(request.POST)
 		if form.is_valid():
 			form.save()
-			return HttpResponseRedirect('/accounts/register_success/')
+			return HttpResponseRedirect('/accounts/register_success')
 
 	args = {}
 	args.update(csrf(request))
 
-	args['form'] = MyRegistrationForm()
+	args['form'] = UserCreationForm()
 
 	return render_to_response('register.html', args)
 
