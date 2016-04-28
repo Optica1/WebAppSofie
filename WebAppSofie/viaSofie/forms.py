d8c00b734571364ab23e5ed7cb5617514a4cd412
nvanheertum <n.vanheertum@gmail.com>
2016-04-28 11:53:40 +0200

    Updating forms.py registration
    Changing variable names for DB entries

--- WebAppSofie/viaSofie/forms.py
+++ WebAppSofie/viaSofie/forms.py
@@ -4,16 +4,16 @@ from django.contrib.auth.forms import UserCreationForm
 
 class MyRegistrationForm(UserCreationForm):
 	email = forms.EmailField(required=True)
-	username = forms.CharField(label='Gebruikersnaam', max_lenght=20, required=True)
-	password1 = forms.CharField(label='Paswoord', max_lenght=20, required=True)
-	password2 = forms.CharField(label='Herhalen paswoord', max_lenght=20, required=True)
-	phonenumber = forms.CharField(label='Telefoonnummer', max_lenght=10, required=True)
-	firstname = forms.CharField(label='Voornaam', max_lenght=15, required=True)
-	lastname = forms.CharField(label='Achternaam', max_lenght=30, required=True)
+	username = forms.CharField(label='Gebruikersnaam', max_length=20, required=True)
+	password1 = forms.CharField(label='Paswoord', max_length=20, required=True)
+	password2 = forms.CharField(label='Herhalen paswoord', max_length=20, required=True)
+	phonenumber = forms.CharField(label='Telefoonnummer', max_length=10, required=True)
+	firstname = forms.CharField(label='Voornaam', max_length=15, required=True)
+	lastname = forms.CharField(label='Achternaam', max_length=30, required=True)
 
 	class Meta:
 		model = User
-		field = ('email', 'username', 'password1', 'password2', 'phonenumber', 'firstname', 'lastname')
+		fields = ('email', 'username', 'password1', 'password2', 'phonenumber', 'firstname', 'lastname')
 
 
 	#overwriting the save method for custom fields
@@ -21,14 +21,13 @@ class MyRegistrationForm(UserCreationForm):
 		user = super(UserCreationForm, self).save(commit=False)# commit false because we do this at end of var assignments
 		user.email = self.cleaned_data['email']#cleaned so all character are valid
 		user.username = self.cleaned_data['username']
-		user.password1 = self.cleaned_data['password1']
-		user.password2 = self.cleaned_data['password2']
+		user.password = self.cleaned_data['password1']
 		user.phonenumber = self.cleaned_data['phonenumber']
-		user.firstname = self.cleaned_data['firstname']
-		user.lastname = self.cleaned_data['lastname']
+		user.first_name = self.cleaned_data['firstname']
+		user.last_name = self.cleaned_data['lastname']
 
 
-		if commit
+		if commit:
 			user.save()
 
 		return user	
