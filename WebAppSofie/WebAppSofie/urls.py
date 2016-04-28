bc4e5cf211bb24f8b35e67e93b2e4ffede55bbd4
sulidude <sulidude@hotmail.com>
2016-04-28 13:28:41 +0200

    updated
    updated deprecated code

--- WebAppSofie/WebAppSofie/urls.py
+++ WebAppSofie/WebAppSofie/urls.py
@@ -15,28 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
-from django.conf.urls import patterns, include, url
+from django.conf.urls import  include, url
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
 
-import settings
-
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    
+    url(r'^', include('viaSofie.urls')),
     # user auth urls
-    url(r'^accounts/login',  'viaSofie.views.login'),
-    url(r'^accounts/auth',  'viaSofie.views.auth_view'),    
-    url(r'^accounts/logout', 'viaSofie.views.logout'),
-    url(r'^accounts/loggedin', 'viaSofie.views.loggedin'),
-    url(r'^accounts/invalid', 'viaSofie.views.invalid_login'),
-    url(r'^accounts/register/$', 'viaSofie.views.register_user'),
-    url(r'^accounts/register_success/$', 'viaSofie.views.register_success'),
-)
-
-if not settings.DEBUG:
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-   
-    urlpatterns += staticfiles_urlpatterns()
+#    url(r'^accounts/login',  'viaSofie.views.login'),
+#    url(r'^accounts/auth',  'viaSofie.views.auth_view'),
+#    url(r'^accounts/logout', 'viaSofie.views.logout'),
+#    url(r'^accounts/loggedin', 'viaSofie.views.loggedin'),
+#    url(r'^accounts/invalid', 'viaSofie.views.invalid_login'),
+#    url(r'^accounts/register/$', 'viaSofie.views.register_user'),
+#    url(r'^accounts/register_success/$', 'viaSofie.views.register_success'),
+]
