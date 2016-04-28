6b82e74a29bdebf1f66cbef26febf359a38a0bf1
nvanheertum <n.vanheertum@gmail.com>
2016-04-28 11:37:13 +0200

    Fix error forms.py
    adding ':' to every class and def

--- WebAppSofie/viaSofie/forms.py
+++ WebAppSofie/viaSofie/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class MyRegistrationForm(UserCreationForm)
+class MyRegistrationForm(UserCreationForm):
 	email = forms.EmailField(required=True)
 	username = forms.CharField(label='Gebruikersnaam', max_lenght=20, required=True)
 	password1 = forms.CharField(label='Paswoord', max_lenght=20, required=True)
@@ -11,13 +11,13 @@ class MyRegistrationForm(UserCreationForm)
 	firstname = forms.CharField(label='Voornaam', max_lenght=15, required=True)
 	lastname = forms.CharField(label='Achternaam', max_lenght=30, required=True)
 
-	class Meta
+	class Meta:
 		model = User
 		field = ('email', 'username', 'password1', 'password2', 'phonenumber', 'firstname', 'lastname')
 
 
 	#overwriting the save method for custom fields
-	def save(self, commit=True)
+	def save(self, commit=True):
 		user = super(UserCreationForm, self).save(commit=False)# commit false because we do this at end of var assignments
 		user.email = self.cleaned_data['email']#cleaned so all character are valid
 		user.username = self.cleaned_data['username']
