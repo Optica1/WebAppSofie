ac9745a51291b9ccbfd461157cef6f3428acc7be
nvanheertum <n.vanheertum@gmail.com>
2016-04-28 12:41:49 +0200

    updating forms.py register
    adding import to form

--- WebAppSofie/viaSofie/forms.py
+++ WebAppSofie/viaSofie/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import *
 
 class MyRegistrationForm(UserCreationForm):
 	email = forms.EmailField(required=True)
