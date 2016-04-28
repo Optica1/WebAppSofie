35aa64aa741811740f4d79df8929e8da54d43e18
sulidude <sulidude@hotmail.com>
2016-04-28 13:28:10 +0200

    updated
    updated deprecated code

--- WebAppSofie/viaSofie/urls.py
+++ WebAppSofie/viaSofie/urls.py
@@ -1,27 +1,18 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import  include, url
 #from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
+from . import views
 
-from django_test.forms import ContactForm1, ContactForm2, ContactForm3
-from django_test.views import ContactWizard
 
-import settings
+urlpatterns =[
 
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    
     # user auth urls
-    url(r'^accounts/login/$',  'django_test.views.login'),
-    url(r'^accounts/auth/$',  'django_test.views.auth_view'),    
-    url(r'^accounts/logout/$', 'django_test.views.logout'),
-    url(r'^accounts/loggedin/$', 'django_test.views.loggedin'),
-    url(r'^accounts/invalid/$', 'django_test.views.invalid_login'),
-)
-
-if not settings.DEBUG:
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-   
-    urlpatterns += staticfiles_urlpatterns()
+    url(r'^$', views.index),
+    url(r'^accounts/login/$',  views.login , name="login"),
+    url(r'^accounts/auth/$',  views.auth_view, name="auth"),
+    url(r'^accounts/logout/$', views.logout, name="logout"),
+    url(r'^accounts/loggedin/$', views.loggedin, name="loggedin"),
+    url(r'^accounts/invalid/$', views.invalid_login , name="invalid")
+]
