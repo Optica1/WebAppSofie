a5dc4ce08522ac8c5208493fd388618010de4d2e
nvanheertum <n.vanheertum@gmail.com>
2016-04-28 11:18:45 +0200

    Updating registration view
    Set own form instead of standard

--- WebAppSofie/viaSofie/views.py
+++ WebAppSofie/viaSofie/views.py
@@ -36,7 +36,7 @@ def logout(request):
 
 def register_user(request):
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = MyRegistrationForm(request.POST)
 		if form.is_valid():
 			form.save()
 			return HttpResponseRedirect('/accounts/register_success')
@@ -44,7 +44,7 @@ def register_user(request):
 	args = {}
 	args.update(csrf(request))
 
-	args['form'] = UserCreationForm()
+	args['form'] = MyRegistrationForm()
 
 	return render_to_response('register.html', args)
 
