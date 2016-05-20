6a12747a659bc957c1b6b45f4ecc46cd220f04fd
Optica1 <tom.dsmet@gmail.com>
2016-05-20 14:35:57 +0200

    Title fix in admin pages

--- WebAppSofie/viaSofie/admin.py
+++ WebAppSofie/viaSofie/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+# from django.contrib.admin import AdminSite
+# from django.utils.translation import ugettext_lazy
 from .models import *
 
 class ClientAdmin(admin.ModelAdmin):
@@ -15,6 +17,18 @@ class UserDetailsInline(admin.StackedInline):
 
 class UserAdmin(UserAdmin):
 	inlines = (UserDetailsInline, )
+
+# class MyAdminSite(AdminSite):
+#     # Text to put at the end of each page's <title>.
+#     site_title = ugettext_lazy('My site admin')
+#
+#     # Text to put in each page's <h1>.
+#     site_header = ugettext_lazy('My administration')
+#
+#     # Text to put at the top of the admin index page.
+#     index_title = ugettext_lazy('Site administration')
+#
+# admin_site = MyAdminSite()
 
 admin.site.register(Aboutpage,AboutpageAdmin)
 admin.site.register(Properties)
