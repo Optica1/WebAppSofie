0ec7e05333f6c4967ca71e0c8076926c5431af59
Liamdelee <liam.delee@student.ap.be>
2016-05-22 22:17:51 +0200

    editing signals
    see if it will save in the database

--- WebAppSofie/viaSofie/signals.py
+++ WebAppSofie/viaSofie/signals.py
@@ -2,18 +2,18 @@ import googlemaps
 import json
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 from my_user_profile_app.models import UserDetails
 
-
 @receiver(pre_save, sender=User)
 def model_pre_change(sender, **kwargs):
-    Userdetails_street=UserDetails.street
-    Userdetails_streetnumber=UserDetails.housenumber
-    Userdatails_postalcode=UserDetails.postalcode
-    Userdetails_city=UserDetails.city
-    location=Userdetails_street+Userdetails_streetnumber+','+Userdatails_postalcode+Userdetails_city
+    Propertie_street=UserDetails.street
+    Propertie_streetnumber=UserDetails.housenumber
+    Propertie_postalcode=UserDetails.postalcode
+    Propertie_city=UserDetails.city
+    location=Propertie_street+Propertie_streetnumber+','+Propertie_postalcode+Propertie_city
 
     gmaps = googlemaps.Client(key='AIzaSyCpFy6NnC1cbEvM8bLRAgzGskxYUeTL-_M')
 
@@ -24,7 +24,7 @@ def model_pre_change(sender, **kwargs):
     latitude = geocode_result[0]["geometry"]["location"]["lat"]
     longitude = geocode_result[0]["geometry"]["location"]["lng"]
 
-    UserDetails.longitude=longitude
-    UserDetails.latitude=latitude
+    Propertie=viaSofie_properties(longitude=longitude, latitude=latitude)
+    Propertie.save()
 
     # full link to google maps geolocation api with right key: https://maps.googleapis.com/maps/api/geocode/json?address=Lindelei35,2620Hemiksem&key=AIzaSyCpFy6NnC1cbEvM8bLRAgzGskxYUeTL-_M
