7aad540c4ad13d7c4bca687d2c7dc011046f1c5a
Liamdelee <liam.delee@student.ap.be>
2016-05-22 17:04:24 +0200

    json querry
    getting the lat an lng out of the json file we retrieved from google
    maps

--- WebAppSofie/viaSofie/signals.py
+++ WebAppSofie/viaSofie/signals.py
@@ -1,3 +1,5 @@
+import googlemaps
+import json
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -11,13 +13,15 @@ def model_pre_change(sender, **kwargs):
     Userdetails_streetnumber=UserDetails.housenumber
     Userdatails_postalcode=UserDetails.postalcode
     Userdetails_city=UserDetails.city
-    googleAPI_key='&key=AIzaSyCpFy6NnC1cbEvM8bLRAgzGskxYUeTL-_M'
+    location=Userdetails_street+Userdetails_streetnumber+','+Userdatails_postalcode+Userdetails_city
 
-    google_maps_geolocation_url='https://maps.googleapis.com/maps/api/geocode/json?address='+Userdetails_street+Userdetails_streetnumber+','+Userdatails_postalcode+Userdetails_city+googleAPI_key
+    gmaps = googlemaps.Client(key='AIzaSyCpFy6NnC1cbEvM8bLRAgzGskxYUeTL-_M')
+
+    # Geocoding an address
+    geocode_result = gmaps.geocode(location)
+
+    # query schrijven voor de json die we hebben terug gekregen om dan de longitude en latitude mee in de database te steken.
+    latitude = geocode_result[0]["geometry"]["location"]["lat"]
+    longitude = geocode_result[0]["geometry"]["location"]["lng"]
+
     # full link to google maps geolocation api with right key: https://maps.googleapis.com/maps/api/geocode/json?address=Lindelei35,2620Hemiksem&key=AIzaSyCpFy6NnC1cbEvM8bLRAgzGskxYUeTL-_M
-
-
-# @receiver(post_save, sender=User)
-# def handle_user_save(sender, instance, created, **kwargs):
-#   if created:
-#     UserDetails.objects.create(user=instance)
