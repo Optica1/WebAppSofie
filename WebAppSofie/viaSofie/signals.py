188b0c09ce1dcd5a2e65f6d75fceab7ee5e99d12
Liamdelee <liam.delee@student.ap.be>
2016-05-22 16:13:41 +0200

    editing signals

--- WebAppSofie/viaSofie/signals.py
+++ WebAppSofie/viaSofie/signals.py
@@ -4,7 +4,17 @@ from django.dispatch import receiver
 
 from my_user_profile_app.models import UserDetails
 
-@receiver(post_save, sender=User)
-def handle_user_save(sender, instance, created, **kwargs):
-  if created:
-    UserDetails.objects.create(user=instance)
+@receiver(pre_save, sender=User)
+def model_pre_change(sender, **kwargs):
+    Userdetails_street=UserDetails.street
+    Userdetails_streetnumber=UserDetails.housenumber
+    Userdatails_postalcode=UserDetails.postalcode
+    Userdetails_city=UserDetails.city
+
+
+
+        https://maps.googleapis.com/maps/api/geocode/json?address=Lindelei35,2620Hemiksem&key=AIzaSyCpFy6NnC1cbEvM8bLRAgzGskxYUeTL-_M
+# @receiver(post_save, sender=User)
+# def handle_user_save(sender, instance, created, **kwargs):
+#   if created:
+#     UserDetails.objects.create(user=instance)
