0cad9d9635f5abd0e17538f32120aec43a2eb424
Liamdelee <liam.delee@student.ap.be>
2016-05-23 14:57:10 +0200

    removing date in models

--- WebAppSofie/viaSofie/models.py
+++ WebAppSofie/viaSofie/models.py
@@ -92,8 +92,8 @@ class Properties(models.Model):
 	extra_information_french = models.TextField()
 	available = models.BooleanField()
 	sold = models.BooleanField()
-	date_created = models.DateField(auto_now_add=True)
-	date_modified = models.DateField(auto_now=True)
+	# date_created = models.DateField(auto_now_add=True)
+	# date_modified = models.DateField(auto_now=True)
 
 class PropertyDocuments(models.Model):
 	property_id = models.ForeignKey(Properties, on_delete=models.PROTECT)
