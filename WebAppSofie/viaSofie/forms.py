6a4f794622a29da939ea6ebb48c96c712831233b
nvanheertum <n.vanheertum@gmail.com>
2016-05-27 15:18:25 +0200

    changing forms

--- WebAppSofie/viaSofie/forms.py
+++ WebAppSofie/viaSofie/forms.py
@@ -45,7 +45,7 @@ class MyRegistrationForm(UserCreationForm):
 
 class MyEbookForm(ModelForm):
 	email = forms.EmailField(required=True)
-	ebook = forms.ModelChoiceField(queryset=Ebook.objects.filter(available=1).values_list('name',flat=True))
+	ebook = forms.ModelChoiceField(queryset=Ebook.objects.filter(available=1).values_list('id','name',flat=True))
 	
 	class Meta:
 		model = EbookRequests
