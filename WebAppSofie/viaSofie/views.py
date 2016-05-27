19a947bc6f2b4b604e8c729b29fefa375a2ea408
nvanheertum <n.vanheertum@gmail.com>
2016-05-27 15:00:35 +0200

    test change view

--- WebAppSofie/viaSofie/views.py
+++ WebAppSofie/viaSofie/views.py
@@ -116,7 +116,7 @@ def ebook(request):
 		if form.is_valid():
 			form.save()
 			return HttpResponseRedirect('/')
-
+		else: return HttpResponseRedirect('/')
 	else:
 		args = {}
 		args.update(csrf(request))
