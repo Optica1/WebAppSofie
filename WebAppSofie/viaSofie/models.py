1308371117a643d6b81942f4ef99d1be8d87d64a
Liamdelee <liam.delee@student.ap.be>
2016-05-30 09:38:00 +0200

    editing save for properties

--- WebAppSofie/viaSofie/models.py
+++ WebAppSofie/viaSofie/models.py
@@ -177,6 +177,7 @@ class Newsletter(models.Model):
 
 @receiver(pre_save, sender=Properties)
 def model_pre_change(sender, **kwargs):
+	Property = Properties.objects.latest('date_modified')
 	Property_street=Property.street
 	Property_streetnumber=Property.housenumber
 	Property_postalcode=Property.postalcode
