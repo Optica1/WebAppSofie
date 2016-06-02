cf6372db6ded675ee902403e27ef2721744d799d
nvanheertum <n.vanheertum@gmail.com>
2016-06-02 12:55:28 +0200

    test

--- WebAppSofie/viaSofie/views.py
+++ WebAppSofie/viaSofie/views.py
@@ -180,7 +180,7 @@ def newsletterSubscribe(request):
 	args['form'] = NewsletterForm
 
 	if request.method == 'POST':
-		form = NewsletterForm(request.POST)
+		form = NewsletterForm
 		if form.is_valid():
 			form.save()
 
@@ -192,7 +192,7 @@ def newsletterUnsubscribe(request):
 	args['form'] = Newsletterun
 
 	if request.method == 'POST':
-		form = NewsletterUnsubscribeForm(request.POST)
+		form = NewsletterUnsubscribeForm
 		if form.is_valid():
 			Newsletter.objects.filter(email=email).delete()
 
