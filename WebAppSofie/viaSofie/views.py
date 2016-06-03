b50ffdd8c314a26c67f80bb43793a66b1894ab9c
Optica1 <tom.dsmet@gmail.com>
2016-06-03 10:29:45 +0200

    homepage fix

--- WebAppSofie/viaSofie/views.py
+++ WebAppSofie/viaSofie/views.py
@@ -16,7 +16,9 @@ import traceback
 
 
 def index(request):
-	return render_to_response('templates/home.html')
+	s = Properties.objects.filter(sale = True, sold = False, available = False).order_by('date_modified')[:4]
+	r = Properties.objects.filter(sale = False, sold = False, available = False).order_by('date_modified')[:4]
+	return render_to_response('templates/home.html', {'Sales':s, 'Rents':r})
 def about(request):
 	try:
 		pages = Aboutpage.objects.all()
