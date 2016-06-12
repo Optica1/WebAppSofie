11d4f53babaa6a62617f2ae1044977a7e252ae1f
Optica1 <tom.dsmet@gmail.com>
2016-06-12 10:25:58 +0200

    changes to media and settings

--- WebAppSofie/WebAppSofie/settings.py
+++ WebAppSofie/WebAppSofie/settings.py
@@ -137,6 +137,8 @@ APPEND_SLASH=False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = '/media/'
 
 TINYMCE_DEFAULT_CONFIG = {
     'plugins': "table,paste,searchreplace",
