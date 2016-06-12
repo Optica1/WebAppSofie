a5e9c3935043d6cd83277445c122ed83775086c2
Liamdelee <liam.delee@student.ap.be>
2016-06-12 11:42:02 +0200

    editing models

--- WebAppSofie/viaSofie/models.py
+++ WebAppSofie/viaSofie/models.py
@@ -12,6 +12,7 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from tinymce import models as tinymce_models
 from datetime import date
+from django.http import HttpResponseRedirect
 
 from django.core.files.storage import FileSystemStorage
 
@@ -129,13 +130,16 @@ class Properties(models.Model):
 	    # Geocoding an address
 		geocode_result = gmaps.geocode(location)
 
-	    # query json
-		latitude = geocode_result[0]["geometry"]["location"]["lat"]
-		longitude = geocode_result[0]["geometry"]["location"]["lng"]
+		if (geocode_result):
+		    # query json
+			latitude = geocode_result[0]["geometry"]["location"]["lat"]
+			longitude = geocode_result[0]["geometry"]["location"]["lng"]
 
-	    # adding longitude and latitude to the database
-		self.longitude = longitude
-		self.latitude = latitude
+		    # adding longitude and latitude to the database
+			self.longitude = longitude
+			self.latitude = latitude
+		else:
+			HttpResponseRedirect('/accounts/invalid')
 		# full link to google maps geolocation api with right key: https://maps.googleapis.com/maps/api/geocode/json?address=Lindelei35,2620Hemiksem&key=AIzaSyCpFy6NnC1cbEvM8bLRAgzGskxYUeTL-_M
 		if not self.id:
 			self.date_created = datetime.datetime.now()
