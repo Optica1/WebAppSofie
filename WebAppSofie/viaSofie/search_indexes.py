9868d1cb1530e6164045a61284a03ea94a268ecc
Liamdelee <liam.delee@student.ap.be>
2016-06-16 14:44:00 +0200

    editing for the search

--- WebAppSofie/viaSofie/search_indexes.py
+++ WebAppSofie/viaSofie/search_indexes.py
@@ -8,11 +8,10 @@ class FaqIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     question = indexes.CharField(model_attr='question')
     answer = indexes.CharField(model_attr='answer')
-    visible = indexes.CharField(model_attr='visible')
 
     def get_model(self):
         return Faq
 
-    # def index_queryset(self, using=None):
-    #     """Used when the entire index for model is updated."""
-    #     return self.get_model().objects.filter(timestamp__lte=timezone.now())
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(timestamp__lte=timezone.now())
--- WebAppSofie/viaSofie/urls.py
+++ WebAppSofie/viaSofie/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^about/viaSofie$', views.about),
     url(r'^ebook', views.ebook),
     url(r'^contact', views.contact),
-    url(r'^search', views.faqs),
+    url(r'^search/', include('haystack.urls')),
     url(r'^partner', views.partner),
     url(r'^faq', views.faq),
     url(r'^info/privacy', views.privacy),
