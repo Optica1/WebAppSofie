9f08299f04ccca0680395bd9430e313fbcb96eda
Liamdelee <liam.delee@student.ap.be>
2016-06-18 10:59:22 +0200

    editing to search on city

--- WebAppSofie/viaSofie/forms.py
+++ WebAppSofie/viaSofie/forms.py
@@ -111,4 +111,4 @@ class NewsletterUnsubscribeForm(forms.Form):
 class PandenSearchForm(SearchForm):
 	# def search(self):
 	def no_query_found(self):
-		return self.searchqueryset.all().order_by('price')
+		return self.searchqueryset.all().order_by('city')
