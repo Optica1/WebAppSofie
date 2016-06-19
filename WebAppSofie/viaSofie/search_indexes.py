6d7ff4cf501998d8c96b96650779057825099bfb
Liamdelee <liam.delee@student.ap.be>
2016-06-19 16:44:10 +0200

    editing search_indexes

--- WebAppSofie/viaSofie/search_indexes.py
+++ WebAppSofie/viaSofie/search_indexes.py
@@ -12,6 +12,8 @@ class PropertiesIndex(indexes.SearchIndex, indexes.Indexable):
     title_dutch = indexes.CharField(model_attr='title_dutch')
     description_dutch = indexes.CharField(model_attr='description_dutch')
     pand_id = indexes.IntegerField(model_attr='id')
+    photo = models.ImageField()
+    priority = models.BooleanField()
 
     def get_model(self):
         return Properties
@@ -23,6 +25,7 @@ class PropertiesIndex(indexes.SearchIndex, indexes.Indexable):
         self.prepared_data = super(PropertiesIndex, self).prepare(object)
 
         # Retrieve the tutorial metas and return the prepared data
-        meta = get_Photo(property_id=object.id)
-        self.prepared_data['photo'] = meta.photo
-        self.prepared_data['priority'] = meta.priority
+        p = Photo.objects.get(property_id=PropertiesIndex.id)
+        # meta = get_Photo(property_id=object.id)
+        self.prepared_data['photo'] = p.photo
+        self.prepared_data['priority'] = p.priority
