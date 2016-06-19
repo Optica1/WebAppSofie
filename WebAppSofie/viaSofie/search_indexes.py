181771a51c0efddb22f87345f149e4d3ee6a49c0
Liamdelee <liam.delee@student.ap.be>
2016-06-19 16:34:42 +0200

    editing search_indexes

--- WebAppSofie/viaSofie/search_indexes.py
+++ WebAppSofie/viaSofie/search_indexes.py
@@ -25,6 +25,6 @@ class PropertiesIndex(indexes.SearchIndex, indexes.Indexable):
         self.prepared_data = super(PropertiesIndex, self).prepare(object)
 
         # Retrieve the tutorial metas and return the prepared data
-        meta = Photo.objects.get(property_id=object.pand_id)
+        meta = Photo.objects.get(property_id=object.id)
         self.prepared_data['photo'] = meta.photo
         self.prepared_data['priority'] = meta.priority
