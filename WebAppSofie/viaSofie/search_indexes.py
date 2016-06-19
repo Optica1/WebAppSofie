4350388e1506882bf5d01827cc8f50d5321f8667
Liamdelee <liam.delee@student.ap.be>
2016-06-19 16:23:06 +0200

    editing search_indexes.py

--- WebAppSofie/viaSofie/search_indexes.py
+++ WebAppSofie/viaSofie/search_indexes.py
@@ -12,7 +12,7 @@ class PropertiesIndex(indexes.SearchIndex, indexes.Indexable):
     title_dutch = indexes.CharField(model_attr='title_dutch')
     description_dutch = indexes.CharField(model_attr='description_dutch')
     pand_id = indexes.IntegerField(model_attr='id')
-    photo = models.ImageField()
+    photo = models.CharField()
     priority = models.IntegerField()
 
     def get_model(self):
