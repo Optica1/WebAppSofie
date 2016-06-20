7acee990ba098e38604a5c719f5cfed3df95136f
Liamdelee <liam.delee@student.ap.be>
2016-06-20 12:29:50 +0200

    editing search_indexes

--- WebAppSofie/viaSofie/search_indexes.py
+++ WebAppSofie/viaSofie/search_indexes.py
@@ -13,20 +13,20 @@ class PropertiesIndex(indexes.SearchIndex, indexes.Indexable):
     description_dutch = indexes.CharField(model_attr='description_dutch')
     pand_id = indexes.IntegerField(model_attr='id')
     photo = models.ImageField()
-    priority = models.BooleanField()
+    # priority = models.BooleanField()
 
     def get_model(self):
         return Properties
 
-    def prepare(self, object):
+    def prepare_photo(self, object):
 
         self.prepared_data = super(PropertiesIndex, self).prepare(object)
 
         # Retrieve the photo url and priority
-        # p = Photo.objects.get(property_id = object.pand_id)
+        p = Photo.objects.get(property_id = object.pand_id)
         # meta = get_Photo(property_id=object.id)
-        self.prepared_data['photo'] = [photo.photo for photo in object.photo.filter(property_id = object.pand_id).filter(priority=1)]
+        # self.prepared_data['photo'] = [photo.photo for photo in object.photo.filter(property_id = object.pand_id).filter(priority=1)]
         # self.prepared_data['priority'] = [photo.priority for photo in object.photo.filter(property_id = object.pand_id)]
-        # self.prepared_data['photo'] = p.photo
+        self.prepared_data['photo'] = p.photo
         # self.prepared_data['priority'] = p.priority
         return self.prepared_data
