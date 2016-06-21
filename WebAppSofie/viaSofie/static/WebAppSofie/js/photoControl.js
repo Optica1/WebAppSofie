fbf14f3bbe35155eb76e6129d4b72edb3e043321
Optica1 <tom.dsmet@gmail.com>
2016-06-21 21:15:32 +0200

    css fixing and javascripting pictures

--- WebAppSofie/viaSofie/static/WebAppSofie/js/photoControl.js
+++ WebAppSofie/viaSofie/static/WebAppSofie/js/photoControl.js
@@ -1,4 +1,27 @@
 function selectPicture(el) {
+  var currentTitle = document.getElementById('priorityImage').title;
+  $("img[title='"+currentTitle+"']").css({"border": "none"});
   $("#priorityImage").attr("src", el.src);
   $("#priorityImage").attr("title", el.title);
+  imageSelected();
+}
+
+function imageSelected() {
+  $("#photolist").find('img').each(function() {
+    var priorityImage = document.getElementById('priorityImage');
+    if (this.title == priorityImage.title){
+      this.style.border = "thick solid #0d3531";
+      return;
+    }
+  });
+}
+
+function unselectImage() {
+  $("#photolist").find('img').each(function() {
+    var priorityImage = document.getElementById('priorityImage');
+    if (this.title == priorityImage.title){
+      this.style.border = "thick solid #0d3531";
+      return;
+    }
+  });
 }
