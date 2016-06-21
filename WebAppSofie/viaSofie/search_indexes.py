d8f365300171f39a6907f7a986b242fee89db765
Liamdelee <liam.delee@student.ap.be>
2016-06-21 09:49:53 +0200

    Revert "editing search_indexes"
    This reverts commit f74dcf38cf24d98bbdcb228d3708be23108d9e7d.

--- WebAppSofie/viaSofie/search_indexes.py
+++ WebAppSofie/viaSofie/search_indexes.py
@@ -13,7 +13,7 @@ class PropertiesIndex(indexes.SearchIndex, indexes.Indexable):
     description_dutch = indexes.CharField(model_attr='description_dutch')
     pand_id = indexes.IntegerField(model_attr='id')
     # photo = models.ImageField()
-    priority = indexes.IntegerField()
+    priority = indexes.BooleanField()
 
     def get_model(self):
         return Properties
