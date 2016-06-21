d280d71a3e9c8a9d14b22854818d4398b44c98d6
Optica1 <tom.dsmet@gmail.com>
2016-06-21 21:55:43 +0200

    jquery performance boost

--- WebAppSofie/viaSofie/static/WebAppSofie/js/photoControl.js
+++ WebAppSofie/viaSofie/static/WebAppSofie/js/photoControl.js
@@ -3,25 +3,10 @@ function selectPicture(el) {
   $("img[title='"+currentTitle+"']").css({"border": "none"});
   $("#priorityImage").attr("src", el.src);
   $("#priorityImage").attr("title", el.title);
-  imageSelected();
+  $("#photolist").find("img[title='"+el.title+"']").css({"border": "thick solid #0d3531"});
 }
 
 function imageSelected() {
-  $("#photolist").find('img').each(function() {
-    var priorityImage = document.getElementById('priorityImage');
-    if (this.title == priorityImage.title){
-      this.style.border = "thick solid #0d3531";
-      return;
-    }
-  });
-}
-
-function unselectImage() {
-  $("#photolist").find('img').each(function() {
-    var priorityImage = document.getElementById('priorityImage');
-    if (this.title == priorityImage.title){
-      this.style.border = "thick solid #0d3531";
-      return;
-    }
-  });
+  var currentTitle = document.getElementById('priorityImage').title;
+  $("#photolist").find("img[title='"+currentTitle+"']").css({"border": "thick solid #0d3531"});
 }
