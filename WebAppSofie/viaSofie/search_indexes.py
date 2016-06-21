64b1cb6f28513b3f31e4f86e6d975b55de41cf82
Liamdelee <liam.delee@student.ap.be>
2016-06-21 09:49:58 +0200

    Revert "editing notes and search_indexes"
    This reverts commit bee1cc6162b334db3a3fc718491657ad72fbec3b.

--- WebAppSofie/viaSofie/search_indexes.py
+++ WebAppSofie/viaSofie/search_indexes.py
@@ -13,19 +13,18 @@ class PropertiesIndex(indexes.SearchIndex, indexes.Indexable):
     description_dutch = indexes.CharField(model_attr='description_dutch')
     pand_id = indexes.IntegerField(model_attr='id')
     # photo = models.ImageField()
-    priority = indexes.BooleanField()
+    priority = models.BooleanField()
 
     def get_model(self):
         return Properties
 
     def prepare_priority(self, object):
-        return obj.propertyid.priority
-        # self.prepared_data = super(PropertiesIndex, self).prepare(object)
-        # # Retrieve the photo url and priority
-        # p = Photo.objects.get(property_id = object.pand_id)
-        # # meta = get_Photo(property_id=object.id)
-        # # self.prepared_data['photo'] = [photo.photo for photo in object.photo.filter(property_id = object.pand_id).filter(priority=1)]
-        # # self.prepared_data['priority'] = [photo.priority for photo in object.photo.filter(property_id = object.pand_id)]
-        # # self.prepared_data['photo'] = p.photo
-        # self.prepared_data['priority'] = p.priority
-        # return self.prepared_data
+        self.prepared_data = super(PropertiesIndex, self).prepare(object)
+        # Retrieve the photo url and priority
+        p = Photo.objects.get(property_id = object.pand_id)
+        # meta = get_Photo(property_id=object.id)
+        # self.prepared_data['photo'] = [photo.photo for photo in object.photo.filter(property_id = object.pand_id).filter(priority=1)]
+        # self.prepared_data['priority'] = [photo.priority for photo in object.photo.filter(property_id = object.pand_id)]
+        # self.prepared_data['photo'] = p.photo
+        self.prepared_data['priority'] = p.priority
+        return self.prepared_data
